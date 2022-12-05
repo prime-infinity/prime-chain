5aeb339009074d79febf4332aa174631d6f61984
prime-infinity <soe.a.prime@gmail.com>
2022-12-05 05:56:42 -0800

    done verify block in python

--- python/chain_version1.py
+++ python/chain_version1.py
@@ -43,8 +43,20 @@ class Chain:
     def create_genesis_chain(self):
         return Block(0, {"amount": 4}, 0)
 
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            previous_block = self.chain[i-1]
+            current_block = self.chain[i]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
+
 
 chain = Chain()
 chain.add_block({"amount": 3, "valid": True})
 chain.add_block({"amount": 11, "valid": True, "water": "yes"})
 chain.show_block()
+print(chain.is_valid())
